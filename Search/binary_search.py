58b8262d458a89a668cd30f72eb0bb3148f52827
Yaroslav <yaros6666@yandex.ru>
2023-03-09 09:24:49 +0300

    Start

--- Search/binary_search.py
+++ Search/binary_search.py
@@ -6,17 +6,17 @@ stop = len(array)-1
 
 class Search:
     def binary_search(self, array, num, start, stop):
-        midle = (stop - start)//2
-        find_ind = start + midle
-        if array[find_ind] == num:
-            return find_ind
-        elif num > array[find_ind]:
-            start+=midle
-            return self.binary_search(array, num, start, stop)
-        elif num < array[find_ind]:
-            stop-=midle
-            return self.binary_search(array, num, start, stop)
+        mid_point = (stop - start)//2 + start
+        if (stop - start) < 0:
+            return -1
+        else:
+            if array[mid_point] == num:
+                return mid_point
+            elif num > array[mid_point]:
+                return self.binary_search(array, num, mid_point + 1, stop)
+            elif num < array[mid_point]:
+                return self.binary_search(array, num, start, mid_point - 1)
 
 
 
-print(Search().binary_search(array,24,0,stop))
+print(Search().binary_search(array,100,0,stop))
